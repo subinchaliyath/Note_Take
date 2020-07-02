bb65c12cc154c9ae35c693c1f8badfd00e724320
Subin Chaliyath <subinchaliyath@gmail.com>
2020-07-02 18:46:46 +0530

    Updates

---
+++ precache-manifest.30d1e98f92539a4b11804a7ad1d5af82.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57332838be164a5624f430a8e9a93298",
+    "url": "/Note_Take/index.html"
+  },
+  {
+    "revision": "e9902c22e09625023510",
+    "url": "/Note_Take/static/js/2.35b7ee19.chunk.js"
+  },
+  {
+    "revision": "be3c7a9b68cd024c6e4d",
+    "url": "/Note_Take/static/js/main.b02f48ab.chunk.js"
+  },
+  {
+    "revision": "07d19c8c2ec8ae5efe63",
+    "url": "/Note_Take/static/js/runtime-main.b033cbac.js"
+  }
+]);
