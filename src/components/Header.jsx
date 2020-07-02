2de787f00b47034b3acea49491400a6c6405dba0
Subin Chaliyath <subinchaliyath@gmail.com>
2020-07-02 18:25:24 +0530

    Take Note React Project-frontend

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteIcon from '@material-ui/icons/EventNote';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon />
+        Take Note
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
